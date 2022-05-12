c1eefe41b1640f7f1ce98261be42c43bd498bcad
James Borden <jmborden@gmail.com>
2022-05-12 11:47:50 -0400

    only download the latest HGNC complete set

--- R/download.R
+++ R/download.R
@@ -8,9 +8,11 @@ fs::dir_create(download_dir)
 
 print("Downloading Files")
 url='http://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/archive/monthly/tsv'
-ftp=stringr::str_replace(url,"http","ftp")
+ftp = stringr::str_replace(url,"http","ftp")
 url |> rvest::read_html() |> 
 rvest::html_elements("a") |> 
 rvest::html_attr("href") |>
 purrr::keep(~ grepl("complete_set",.x)) |>
-walk(~ download.file(url=file.path(ftp,.x),destfile=(file.path(download_dir,.x))))
+sort(decreasing=TRUE) |>
+purrr::pluck(1) |>
+purrr::walk(~ download.file(url=file.path(ftp,.x),destfile=(file.path(download_dir,.x))))
--- R/process.R
+++ R/process.R
@@ -1,15 +1,8 @@
-library(purrr)
 library(fs)
-install.packages("pkgcond", repos = "http://cran.us.r-project.org")
-quiet <- pkgcond::suppress_conditions
+library(arrow)
 
 download_dir <- "download"
 data_dir <- "data"
 fs::dir_create(data_dir)
-files <- fs::dir_ls(download_dir) |>
-    map(~ {
-        df <- vroom::vroom(.x) |> quiet()
-        df
-    }) |>
-    dplyr::bind_rows() |>
-    arrow::write_parquet(sink = file.path(data_dir, "hgnc_complete_set.parquet"))
+arrow::write_parquet(vroom::vroom(fs::dir_ls(download_dir)[[1]]),
+    sink = file.path(data_dir, "hgnc_complete_set.parquet"))
