37a5693e49a1ac32c7fbaaa4bb95ee35fab21d1e
John Shaffer <john@insilica.co>
2023-09-26 13:00:50 -0500

    Rename data dir to brick

--- R/process.R
+++ R/process.R
@@ -2,7 +2,7 @@ library(fs)
 library(arrow)
 
 download_dir <- "download"
-data_dir <- "data"
+data_dir <- "brick"
 fs::dir_create(data_dir)
 arrow::write_parquet(vroom::vroom(fs::dir_ls(download_dir)[[1]]),
     sink = file.path(data_dir, "hgnc_complete_set.parquet"))
