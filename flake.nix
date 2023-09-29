47a65603189df72ce7ca2b637c7ee8f34536fb28
John Shaffer <john@insilica.co>
2023-09-29 09:54:08 -0500

    Use biobricks-ai/dev-shell

--- flake.nix
+++ flake.nix
@@ -1,46 +1,9 @@
 {
   description = "HGNC biobrick";
 
-  inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
-    flake-utils.url = "https://flakehub.com/f/numtide/flake-utils/*.tar.gz";
-    biobricks-R = {
-      url = "github:biobricks-ai/biobricks-R";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    hdt-cpp = {
-      url = "github:insilica/nix-hdt";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs = { dev-shell.url = "github:biobricks-ai/dev-shell"; };
 
-  outputs = { self, nixpkgs, flake-utils, biobricks-R, hdt-cpp }:
-    flake-utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; };
-      let
-        # tmducken requires duckdb 0.8.1 or later, and nixos-23.05 only had 0.7
-        duckdb-version = "0.8.1";
-        duckdb = (pkgs.duckdb.overrideAttrs (oldAttrs: rec {
-          version = duckdb-version;
-          src = fetchFromGitHub {
-            owner = "duckdb";
-            repo = "duckdb";
-            rev = "v${duckdb-version}";
-            sha256 = "sha256-LEv9yURkYvONObTbIA4CS+umwCRMH8gRQaDtzbCzID4=";
-          };
-        }));
-      in {
-        devShells.default = mkShell {
-          buildInputs = [
-            biobricks-R.packages.${system}.rEnv
-            clojure
-            hdt-cpp.packages.${system}.default
-            duckdb
-            jdk
-          ];
-          env = { DUCKDB_HOME = "${duckdb}/lib"; };
-        };
-      });
+  outputs = { self, dev-shell }: {
+    devShells = dev-shell.devShells;
+  };
 }
